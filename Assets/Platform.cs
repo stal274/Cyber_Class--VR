4fe6c1552059be90e91016a41f4fdc599b1a68d5
Павел <plyushhaj2@gmail.com>
2022-07-17 15:09:56 +0300

    Init commit

---
+++ Assets/Platform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player")
+			other.transform.parent = transform;
+	}
+
+	private void OnTriggerExit(Collider other) {
+		if (other.tag == "Player")
+		{
+			other.transform.parent = null;
+			other.transform.localScale = Vector3.one;
+		}
+	}
+
+}
